901c98a0ad1651df319935f930e01ffe59951306
gjongenelen <gino@jongenelen.net>
2025-03-30 16:04:26 +0200

    use attr prefix

--- custom_components/teconnect/sensor.py
+++ custom_components/teconnect/sensor.py
@@ -16,7 +16,7 @@ async def async_setup_entry(hass, entry, async_add_entities):
         TEConnectSensor(api, "Current Temperature", lambda d: d["data"][0]["temps"]["Probe_1"] / 10, "temp_probe_1", "°C"),
         TEConnectSensor(api, "Set Temperature", lambda d: d["data"][0]["params"]["SEt"] / 10, "set_temp", "°C"),
         TEConnectSensor(api, "Hysteresis", lambda d: d["data"][0]["params"]["Hy"] / 10, "hysteresis", "°C"),
-        TEConnectClimate(api, "climate_teconnect", "°C"),
+        TEConnectClimate(api, "°C"),
         TEConnectBinarySensor(api, "Defrost", lambda d: d["data"][0]["status"]["Defrost"] == 1, "defrost_active"),
         TEConnectBinarySensor(api, "Cooling", lambda d: d["data"][0]["status"]["Cooling"] == 1, "cooling_active")
     ]
@@ -61,37 +61,13 @@ class TEConnectSensor(SensorEntity):
         self._state = self._value_fn(data)
 
 class TEConnectClimate(ClimateEntity):
-    def __init__(self, api, unique_id, unit):
+    def __init__(self, api, unit):
         self.api = api
-        self._unique_id = unique_id
+        self._attr_unique_id = None
+        self._attr_name = "TEConnect Climate"
         self._attr_temperature_unit = unit
-        self._current_temperature = None
-        self._target_temperature = None
-        self._hvac_mode = HVACMode.OFF
-
-    @property
-    def name(self):
-        return "TEConnect Climate"
-
-    @property
-    def unique_id(self):
-        return self._unique_id
-
-    @property
-    def current_temperature(self):
-        return self._current_temperature
-
-    @property
-    def target_temperature(self):
-        return self._target_temperature
-
-    @property
-    def hvac_mode(self):
-        return self._hvac_mode
-
-    @property
-    def supported_features(self):
-        return ClimateEntityFeature.TARGET_TEMPERATURE
+        self._attr_hvac_mode = HVACMode.OFF
+        self._attr_supported_features = ClimateEntityFeature.TARGET_TEMPERATURE
 
     async def async_set_temperature(self, **kwargs):
         temperature = kwargs.get("temperature")
@@ -101,9 +77,9 @@ class TEConnectClimate(ClimateEntity):
 
     async def async_update(self):
         data = await self.api.fetch_data()
-        self._current_temperature = data["data"][0]["temps"]["Probe_1"] / 10
-        self._target_temperature = data["data"][0]["params"]["SEt"] / 10
-        self._hvac_mode = HVACMode.HEAT_COOL if data["data"][0]["status"]["OnOff"] == 1 else HVACMode.OFF
+        self._attr_current_temperature = data["data"][0]["temps"]["Probe_1"] / 10
+        self._attr_target_temperature = data["data"][0]["params"]["SEt"] / 10
+        self._attr_hvac_mode = HVACMode.HEAT_COOL if data["data"][0]["status"]["OnOff"] == 1 else HVACMode.OFF
 
 class TEConnectBinarySensor(BinarySensorEntity):
     def __init__(self, api, name, value_fn, unique_id):
